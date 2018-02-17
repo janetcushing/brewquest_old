b4735272f06af37a96d72f4593b6aff151a9133e
janetcushing <janet.cushing@gmail.com>
2018-02-17 08:55:02 -0500

    moved pages folder out of components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navpills from "./components/Navpills";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Search from "./components/pages/Search";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Search from "./pages/Search";
 
 
 const App = () =>
