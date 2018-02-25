59be351aa79c30e8734377e616c23a39d2fc5104
janetcushing <janet.cushing@gmail.com>
2018-02-24 19:03:15 -0500

    adding components to search page

---
+++ client/src/components/MyAwesomeReactComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Navpills from "./Navpills";
+ 
+const MyAwesomeReactComponent = () => (
+  <div>
+  <Navpills />
+  <RaisedButton label="Default" />
+  </div>
+);
+ 
+export default MyAwesomeReactComponent;
