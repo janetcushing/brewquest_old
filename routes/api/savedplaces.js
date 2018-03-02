2d0a037e585bcaed91f6fd9e39cc33f255cd0623
Lauren Wilson <Lauren@Laurens-MacBook.local>
2018-03-01 20:04:01 -0500

    created saved places page, added to the utils->API, created the data routes, and created the saved places controller

---
+++ routes/api/savedplaces.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const savedPlacesController = require("../../controllers/savedPlacesController");
+
+// Matches with "/api/savedplaces"
+router.route("/")
+  .get(savedPlacesController.findAll)
+  .post(savedPlacesController.create);
+
+// Matches with "/api/savedplaces/:id"
+router
+  .route("/:id")
+  .get(savedPlacesController.findById)
+  .put(savedPlacesController.update)
+  .delete(savedPlacesController.remove);
+
+module.exports = router;
