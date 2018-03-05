d5f4adc95ba917b20696c34c9761339035f6d042
James Roth <jkltroth@gmail.com>
2018-03-04 22:43:23 -0500

    Buggy searchField persistence created.

--- client/src/components/SearchField.js
+++ client/src/components/SearchField.js
@@ -1,31 +1,40 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import {blue50, grey50, grey900} from 'material-ui/styles/colors';
+import { blue50, grey50, grey900 } from 'material-ui/styles/colors';
 
 const styles = {
     hintStyle: {
-      color: blue50,
+        color: blue50,
     },
     buttonStyle: {
         backgroundColor: grey50,
         labelColor: grey900,
-      }
-  };
-  
+    }
+};
 
-const SearchField = () => (
+
+const SearchField = props => (
     <div>
         <TextField
             name="SearchforPlaces"
             hintText="Search for Places Here"
             hintStyle={styles.hintStyle}
+            value={props.searchLocation}
+            onChange={props.handleSearchLocationChange}
         /><br />
-        <RaisedButton
-            label="Search"
-            buttonStyle={styles.buttonStyle}
-            onTouchTap={this.handleTouchTap}
-        />
+        <Link to={{
+            pathname: '/search',
+            state: { searchLocation: props.searchLocation }
+        }}>
+            <RaisedButton
+                label="Search"
+                buttonStyle={styles.buttonStyle}
+                onTouchTap={this.handleTouchTap}
+                onClick={(event) => props.handleFormSubmit(event)}
+            />
+        </Link>
     </div>
 );
 
