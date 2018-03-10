69740398685b342f56c91b22913ea0ec4daa46f3
Lauren Wilson <Lauren@Laurens-MacBook.local>
2018-03-10 11:46:29 -0500

    got card actions aligned right and removed some of the unnecessary place detail that I was pulling in so I can reorganize and create the expandable cards. Also cleaned up some code we weren't uising that was commented out

--- client/src/pages/SavedPlaces.js
+++ client/src/pages/SavedPlaces.js
@@ -8,8 +8,7 @@ import { hasBeenThere } from "../utils/hasbeenthereapi";
 import { Link } from "react-router-dom";
 import { SavedListItem, SavedList } from "../components/SavedPlacesList";
 import Clear from 'material-ui/svg-icons/content/clear';
-import { Card, CardActions, CardTitle, CardText, CardHeader } from 'material-ui/Card';
-import Check from 'material-ui/svg-icons/navigation/check';
+import { Card, CardActions, CardTitle } from 'material-ui/Card';
 import Check_box_outline_blank from 'material-ui/svg-icons/toggle/check-box-outline-blank'
 import Check_box from 'material-ui/svg-icons/toggle/check-box'
 import Info_outline from 'material-ui/svg-icons/action/info-outline'
@@ -77,7 +76,6 @@ class SavedPlaces extends Component {
               {this.state.results.map(result => {
                 return (
                   <Card>
-                    
                     <SavedListItem key={result._id}>
                     <Row>
                       <Col size="sm-8">
