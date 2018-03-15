571537fddebc2b11815d43691d56ce94501942dc
janetcushing <janet.cushing@gmail.com>
2018-03-15 11:53:51 -0400

    committing

--- client/src/utils/AuthService.js
+++ client/src/utils/AuthService.js
@@ -5,8 +5,7 @@ const ID_TOKEN_KEY = 'id_token';
 const ACCESS_TOKEN_KEY = 'access_token';
 const CLIENT_ID = 'hBUrEY7ugr1dCF8SatxQiOnIVVW4c5ia';
 const CLIENT_DOMAIN = 'beer-quest.auth0.com';
-// const REDIRECT = process.env.CALLBACK_URI || 'http://localhost:3000/callback';
-const REDIRECT = process.env.CALLBACK_URI || 'https://beer-quest.herokuapp.com/callback';
+const REDIRECT = process.env.CALLBACK_URI || 'http://localhost:3000/callback';
 const SCOPE = 'openid profile';
 const AUDIENCE = 'https://beer-quest.auth0.com/userinfo';
 
